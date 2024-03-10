c96678d26ebd8bf2b9cc07cbda33daa963d04c1b
tung <tungvippro456@gmail.com>
2024-03-10 16:33:48 +0700

    LogoFahasa

--- book-store/public/js/app.js
+++ book-store/public/js/app.js
@@ -1,11 +1,21 @@
 class MenuDashboard extends React.Component {
   render() {
     return (
-      <div>1</div>
+      <div class="fhs-header-top-second-bar">
+        <FahasaLogo />
+      </div>
     );
   }
 }
-
+class FahasaLogo extends React.Component{
+  render(){
+    return(
+      <div class="fhs_mouse_point" onclick="location.href = '/';" id="logoFahasa" title="FAHASA.COM">        
+        <img src="images/fahasa-logo.png" />
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <MenuDashboard />,
